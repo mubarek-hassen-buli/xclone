1d11e701a710caf78108cc79d3e685a22ae17563
Mubarek <mubirimuru@gmail.com>
2025-08-25 00:05:10 +0300

    db models created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,12 @@
 import express from "express";
-
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 const app = express();
 
-app.listen(process.env.PORT || 5001, () => console.log("Server running on PORT 5001"))
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(ENV.PORT || 5001, () => console.log("Server running on PORT 5001"));

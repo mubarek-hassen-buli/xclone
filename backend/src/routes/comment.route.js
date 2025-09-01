9a70721239b99ff51985f03df2cc2b17bb07ac02
Mubarek <mubirimuru@gmail.com>
2025-09-01 22:46:58 +0300

    added comment routes

---
+++ backend/src/routes/comment.route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {getComment,createComment,deleteComment} from "../controllers/comment.controller.js"
+
+
+const router = express.Router();
+
+//public routes
+
+router.get("/post/:postId",getComment)
+
+//protected routes
+router.post("/post/:postId",protectRoute,createComment)
+router.delete("/:commentId",protectRoute,deleteComment)
+
+export default router;

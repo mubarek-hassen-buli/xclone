93f156c604a80ac65c1b68e2cfcdd9604d8b6512
Mubarek <mubirimuru@gmail.com>
2025-09-05 00:03:28 +0300

    small change

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -8,9 +8,9 @@ import {
 } from "../controllers/user.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
-
+//public routes
 router.get("/profile/:username", getUserProfile);
-
+//protected routes
 router.post("/sync", protectRoute, syncUser);
 router.post("/me", protectRoute, getCurrentUser);
 router.put("/profile", protectRoute, updateProfile);

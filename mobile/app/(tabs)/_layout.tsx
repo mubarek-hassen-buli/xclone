34e5a88597175dbe0982634eb76197c1caa34786
Mubarek <mubirimuru@gmail.com>
2025-09-09 22:26:47 +0300

    tabs setup done

---
+++ mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,74 @@
+import { Tabs, Redirect } from "expo-router";
+import { Feather } from "@expo/vector-icons";
+import React from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context"; // to make it visible or not to make it under display
+import { useAuth } from "@clerk/clerk-expo";
+const TabsLayout = () => {
+  const insets = useSafeAreaInsets();
+  const { isSignedIn } = useAuth();
+  if (!isSignedIn) return <Redirect href="/(auth)" />;
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#1DA1F2",
+        tabBarInactiveTintColor: "#657786",
+        tabBarStyle: {
+          backgroundColor: "#fff",
+          borderTopWidth: 1,
+          borderTopColor: "#E1E8ED",
+          height: 50 + insets.bottom,
+          paddingTop: 8,
+        },
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="search" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          title: "",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="bell" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="messages"
+        options={{
+          title: "",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="mail" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
---
+++ mobile/app/(tabs)/messages.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MessagesScreen = () => {
+  return (
+    <View>
+      <Text>M</Text>
+    </View>
+  )
+}
+
+export default MessagesScreen
